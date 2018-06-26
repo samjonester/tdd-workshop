dc503136de08658695b2233cc1fe907bad4a5f7f
Sam Jones <sam@samljones.com>
2018-06-26 13:50:52 -0400

    Mocks in Java

---
+++ java/src/main/java/Recipient.java
@@ -0,0 +1,8 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Recipient {
+
+    public String name() {
+        throw new NotImplementedException();
+    }
+}
